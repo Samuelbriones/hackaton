823135b2672cf9451f9a1100301fbedb116d82e4
Samuelbriones <samuelbriones558@gmail.com>
2024-11-14 16:50:33 -0500

    hola

--- APP/Core/urls.py
+++ APP/Core/urls.py
@@ -6,6 +6,5 @@ app_name = 'Core'
 urlpatterns = [
     path('', Main.MainTemplateView.as_view(), name='index'),
     path('map/',Main.TiendaTemplateView.as_view(), name='map'),
-    path('fertilizante/',Main.FertilizanteTemplateView.as_view(), name= 'fertilizante'),
     path('Team/',Main.TeamTemplateView.as_view(), name= 'team')
 ]
