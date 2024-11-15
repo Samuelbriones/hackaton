8e6a337f16762256f123793d82825f07f8652b95
agomezp2003 <agomezp2003@gmail.com>
2024-11-15 16:03:32 +0000

    a

--- APP/Core/view/Main.py
+++ APP/Core/view/Main.py
@@ -1,7 +1,29 @@
 from django.views.generic import TemplateView
+from APP.Core.models import EnergyData
+from django.db.models import Max
+import json
+
 
 class MainTemplateView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        latest_year = EnergyData.objects.aggregate(Max('year'))['year__max']
+        items = EnergyData.objects.filter(year=latest_year).values(
+        'entity',
+        'electricity_from_fossil_fuels',
+        'electricity_from_nuclear',
+        'co2_emissions',
+        'renewables_equivalent'
+        )
+        try:
+            context["countries"] = list(items)
+            context["json"] = json.dumps(list(items))
+        except EnergyData.DoesNotExist:
+            context["countries"] = {"error": "data not found"}
+        return context
+
     
 class TiendaTemplateView(TemplateView):
     template_name = 'map.html'
