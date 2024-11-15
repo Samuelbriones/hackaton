d2b1f5fe8f2b7c65096f6f2047f9a51679084ed2
Willian <konradlisthrop@gmail.com>
2024-11-15 08:00:06 -0500

    .

--- prediction.py
+++ prediction.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os
+
+model = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'co2_model.pkl') 
 
 # Datos de ejemplo para predicción
 new_data = {
@@ -6,7 +9,7 @@ new_data = {
     'Access to clean fuels for cooking' : [100.0],
     'Electricity from fossil fuels (TWh)': [1.0],
     'Electricity from renewables (TWh)': [20.0],
-    'Electricity from nuclear (TWh)': [20.0],
+    'Electricity from nuclear (TWh)': [10.0],
     'Primary energy consumption per capita (kWh/person)': [1000.0],
 }
 
@@ -14,7 +17,7 @@ new_data_df = pd.DataFrame(new_data)
 
 import joblib
 
-loaded_model = joblib.load('co2_model.pkl')
+loaded_model = joblib.load(model)
 
 predictions = loaded_model.predict(new_data_df)
 
